b69459b4d1591fb125ac818aa7af4eba39bd0f5d
MULLA IRSHAD <mullairshad333@gmail.com>
2026-08-17 08:45:39 +0530

    Time: 2 ms (91.98%), Space: 47.9 MB (20.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int l=0;
-        int r=0;
-        while(r<nums.length)
+        for(int r=0;r<nums.length;r++)
         {
             if(nums[r]!=0)
             {
                 int temp=nums[r];
                 nums[r]=nums[l];
                 nums[l]=temp;
-                r++;
                 l++;
             }      
-            else
-            {
-                r++;
-            }
         }
     }
 }
