60eba70cff1fb80100cb413d6065bbe879aea95b
MULLA IRSHAD <mullairshad333@gmail.com>
2026-09-12 16:13:01 +0530

    Time: 17 ms (16.78%), Space: 46.6 MB (24.97%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        int n=s.length();
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        for(char ch:s.toCharArray())
+        {
+            if(map.containsKey(ch))
+            {
+                map.put(ch,map.get(ch)+1);
+            }
+            else
+            {
+                map.put(ch,1);
+            }
+        }
+        for(char ch:t.toCharArray())
+        {
+            if(!map.containsKey(ch))
+            {
+                return false;
+            }
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch)==0)
+            {
+                map.remove(ch);
+            }
+        }
+        return map.isEmpty();
+    }
+}
