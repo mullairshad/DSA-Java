84fdad6fed4b79d39674c849a538db86ed6ea976
MULLA IRSHAD <mullairshad333@gmail.com>
2026-09-18 11:12:21 +0530

    Time: 1 ms (96.45%), Space: 42 MB (62.5%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,11 +1,19 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<num;i++)
+        if(num==1)
+        {
+            return false;
+        }
+        int sum=1;
+        for(int i=2;i*i<=num;i++)
         {
             if(num%i==0)
             {
                 sum=sum+i;
+                if(num/i!=i)
+                {
+                    sum=sum+(num/i);
+                }
             }
         }
         return sum==num;
